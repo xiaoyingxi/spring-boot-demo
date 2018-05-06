cab2e80b6a5e64224549176b47b51796e1c52e9b
dave <liucl@i-counting.cn>
2018-05-06 13:35:19 +0800

    add travis.yml for auto test

--- src/test/java/com/yuuyoo/demo/MockClient.java
+++ src/test/java/com/yuuyoo/demo/MockClient.java
@@ -1,6 +1,5 @@
 package com.yuuyoo.demo;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.core.io.ClassPathResource;
@@ -8,18 +7,18 @@ import java.io.IOException;
 
 public class MockClient {
 
-  public static void main(String[] args) throws Exception {
-    WireMock.configureFor(8080);
-    WireMock.removeAllMappings();
-
-    mock("/order/1", "01");
-    mock("/order/2", "02");
-  }
-
-  private static void mock(String url, String file) throws IOException {
-    ClassPathResource resource = new ClassPathResource("mock/response/" + file + ".txt");
-    String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(), "\n");
-    WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url)).willReturn(WireMock.aResponse().withBody(content).withStatus(200)));
-  }
+//  public static void main(String[] args) throws Exception {
+//    WireMock.configureFor(8080);
+//    WireMock.removeAllMappings();
+//
+//    mock("/order/1", "01");
+//    mock("/order/2", "02");
+//  }
+//
+//  private static void mock(String url, String file) throws IOException {
+//    ClassPathResource resource = new ClassPathResource("mock/response/" + file + ".txt");
+//    String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(), "\n");
+//    WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url)).willReturn(WireMock.aResponse().withBody(content).withStatus(200)));
+//  }
 
 }
